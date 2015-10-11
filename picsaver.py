498c8d8682892389983403d89717b58d3cbb7ff5
Damanpreet <daman.4880@gmail.com>
2015-10-11 20:23:59 +0530

    Added function to download images of particular user by passing his/her username.

--- picsaver.py
+++ picsaver.py
@@ -5,8 +5,8 @@ import shutil
 
 auth_url = "https://api.instagram.com/oauth/authorize/?client_id="+ secrets.client_id + "&redirect_uri=http://www.example.com&response_type=token"
 
-def download_all_imgs():
-    url = "https://api.instagram.com/v1/users/self/media/recent/?access_token="
+def download_all_imgs(user_id):
+    url = "https://api.instagram.com/v1/users/" + user_id + "/media/recent/?access_token="
     r = requests.get(url+secrets.access_token)
     data = r.json()
     items = len(data['data'])
@@ -14,9 +14,19 @@ def download_all_imgs():
     for i in range(items):
         link = data['data'][i]['images']['standard_resolution']['url']
         r = requests.get(link, stream = True)
+        print "Saving image "+ str(z+1) + "."
         with open(str(z)+'.jpg','wb') as f:
             shutil.copyfileobj(r.raw,f)
+        print "Done."
         z=z+1
 
+def download_all_imgs_user(name):
+    u = "https://api.instagram.com/v1/users/search?q="+ name +"&access_token="
+    r_ = requests.get(u+secrets.access_token)
+    d = r_.json()
+    user_id = d['data'][0]['id']
+    download_all_imgs(user_id)
 
+# function calls.
+#download_all_imgs_user(raw_input("Enter the Username "))
 #download_all_imgs()
