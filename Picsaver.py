85513faeba9ca1fafe9279d3dce53126f6819fa4
Damanpreet <daman.4880@gmail.com>
2015-10-11 19:11:51 +0530

    Added function download_all_imgs() to download all of user's images.

--- Picsaver.py
+++ Picsaver.py
@@ -1,9 +1,22 @@
 import sys
 import secrets
 import requests
-
-#username = sys.argv[1]
-#password = sys.argv[2]
+import shutil
 
 auth_url = "https://api.instagram.com/oauth/authorize/?client_id="+ secrets.client_id + "&redirect_uri=http://www.example.com&response_type=token"
 
+def download_all_imgs():
+    url = "https://api.instagram.com/v1/users/self/media/recent/?access_token="
+    r = requests.get(url+secrets.access_token)
+    data = r.json()
+    items = len(data['data'])
+    z = 0
+    for i in range(items):
+        link = data['data'][i]['images']['standard_resolution']['url']
+        r = requests.get(link, stream = True)
+        with open(str(z)+'.jpg','wb') as f:
+            shutil.copyfileobj(r.raw,f)
+        z=z+1
+
+
+#download_all_imgs()
