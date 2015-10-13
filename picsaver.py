8bf4f3475baf2f0b8a5485a294fa745f63a3a098
= <=daman.4880@gmail.com>
2015-10-13 16:31:30 +0530

    Edited code to save Images in a new directory.

--- picsaver.py
+++ picsaver.py
@@ -2,8 +2,14 @@ import sys
 import secrets
 import requests
 import shutil
+import os
 
 auth_url = "https://api.instagram.com/oauth/authorize/?client_id="+ secrets.client_id + "&redirect_uri=http://www.example.com&response_type=token"
+path = os.getcwd()
+
+if os.path.exists('images') == False:
+    os.makedirs('images')
+path = path + '/images/'
 
 def download_all_imgs(user_id):
     url = "https://api.instagram.com/v1/users/" + user_id + "/media/recent/?access_token="
@@ -12,6 +18,7 @@ def download_all_imgs(user_id):
     items = len(data['data'])
     z=1
     y=0
+    print "Saving images in directory : " + path[0:len(path)-1]
     print "Total Number of Images are " + str(items) + "."
     for i in range(items):
         link = data['data'][i]['images']['standard_resolution']['url']
@@ -25,7 +32,7 @@ def download_all_imgs(user_id):
 
         print "Saving image "+ img_name
         print "Image number " + str(z) + "."
-        with open(img_name + ".jpg",'wb') as f:
+        with open(path + img_name + ".jpg",'wb') as f:
             shutil.copyfileobj(r.raw,f)
         print "Done."
         z+=1
@@ -42,5 +49,5 @@ def get_user_id(name):
 
 
 # function calls.
-download_all_imgs_user(raw_input("Enter the Username "))
+#download_all_imgs_user(raw_input("Enter the Username "))
 #download_all_imgs()
