70f528351c6d860c49f5b2934734e86e407757a1
= <=daman.4880@gmail.com>
2015-10-20 20:33:58 +0530

    Improved code readability.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is a Program/script that will use Instagram API to download all images of t
 
 Usage.
 Run the picsaver.py file from terminal and enter the username to download images of the user.
-It will save the images in a new directory named "images" with the caption of images as image name.
+It will save the images in a new directory named "Images" with timestamp of the image as image's name.
 
 NOTE- it needs some access codes which are placed in secrets.py file and which are not public.
 
--- picsaver.py
+++ picsaver.py
@@ -5,27 +5,32 @@ import shutil
 import os
 import datetime
 
-auth_url = "https://api.instagram.com/oauth/authorize/?client_id="+ secrets.client_id + "&redirect_uri=http://www.example.com&response_type=token"
+auth_url = ("https://api.instagram.com/oauth/authorize/?client_id=%s"
+            "&redirect_uri=http://www.example.com"
+            "&response_type=toke" % secrets.client_id)
 path = os.getcwd()
 
-if os.path.exists('images') == False:
+if os.path.exists('images') is False:
     os.makedirs('images')
 path = path + '/images/'
 
 
-def download_all_imgs(user_id):
-    user_info = "https://api.instagram.com/v1/users/" + user_id + "/?access_token="
+def download_all_imgs(name):
+    user_id = get_user_id(name)
+    user_info = ("https://api.instagram.com/v1/users/%s"
+                 "/?access_token=" % user_id)
     info = requests.get(user_info + secrets.access_token)
     u_info = info.json()
     total_media = u_info['data']['counts']['media']
 
-    if total_media%20!=0:
-        steps = total_media/20 + 1
+    if total_media % 20 != 0:
+        steps = total_media / 20 + 1
     else:
         steps = total_media/20
 
-    url = "https://api.instagram.com/v1/users/" + user_id + "/media/recent/?access_token="
-    z=1
+    url = ("https://api.instagram.com/v1/users/%s"
+           "/media/recent/?access_token=" % user_id)
+    z = 1
     print "Total Number of Images are " + str(total_media) + "."
     print "Saving images in directory : " + path[0:len(path)-1]
     for j in range(steps):
@@ -34,30 +39,30 @@ def download_all_imgs(user_id):
 
         for i in range(len(data['data'])):
             link = data['data'][i]['images']['standard_resolution']['url']
-            r = requests.get(link, stream = True)
-            img_name = datetime.datetime.fromtimestamp(int(data['data'][i]['created_time'])).strftime('%Y-%m-%d %H:%M:%S') + '.jpg'
+            r = requests.get(link, stream=True)
+            img_name = datetime.datetime.fromtimestamp(
+                        int(data['data'][i]['created_time'])
+                        ).strftime('%Y-%m-%d %H:%M:%S') + '.jpg'
 
-            print "Saving image "+ img_name
+            print "Saving image " + img_name
             print "Image number " + str(z) + "."
-            with open(path + img_name + '.jpg','wb') as f:
-                shutil.copyfileobj(r.raw,f)
-            z=z+1
+            with open(path + img_name + '.jpg', 'wb') as f:
+                shutil.copyfileobj(r.raw, f)
+            z += 1
         if len(data['pagination']) is 0:
             break
         url = data['pagination']['next_url']
 
 
-def download_all_imgs_user(name):
-    download_all_imgs(get_user_id(name))
-
-
 def get_user_id(name):
-    u = "https://api.instagram.com/v1/users/search?q="+ name +"&access_token="
-    r_ = requests.get(u+secrets.access_token)
+    u = ("https://api.instagram.com/v1/users/search?q=%s"
+         "&access_token=" % name)
+    r_ = requests.get(u + secrets.access_token)
     d = r_.json()
     user_id = d['data'][0]['id']
     return user_id
 
 
-#download_all_imgs()
-download_all_imgs_user(raw_input("Enter Your Username: "))
+# download_all_imgs()
+
+download_all_imgs(raw_input("Enter Your Username: "))
